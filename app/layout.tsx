23ffe7e4c7b8351741822f4d34f4ce5c1cb6441c
Victoria Samoilenko <vikki@Mac.lan>
2026-04-07 20:25:48 +0200

    feat: update favicons with new planner icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,16 @@ const plusJakarta = Plus_Jakarta_Sans({
 export const metadata: Metadata = {
   title: "Personal Planner",
   description: "Victoria's personal dashboard",
+  icons: {
+    icon: [
+      { url: "/img/favicons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/img/favicons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/img/favicons/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/img/favicons/favicon-128x128.png", sizes: "128x128", type: "image/png" },
+      { url: "/img/favicons/favicon-196x196.png", sizes: "196x196", type: "image/png" },
+    ],
+    apple: { url: "/img/favicons/favicon-196x196.png", sizes: "196x196", type: "image/png" },
+  },
 };
 
 export default function RootLayout({
